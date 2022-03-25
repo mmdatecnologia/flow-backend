62f18d9650b8db10212011356f2f062122f29312
Mateus Anjos <mmdatecnologia@gmail.com>
2022-03-25 04:05:49 -0300

    refactor(test): define sut test

--- test/app.service.spec.ts
+++ test/app.service.spec.ts
@@ -1,22 +1,21 @@
 import { Test, TestingModule } from '@nestjs/testing'
-import { AppController } from '@app/app.controller'
 import { AppService } from '@app/app.service'
 
-describe('AppController', () => {
-  let appController: AppController
+describe('AppService', () => {
+  let appService: AppService
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
-      controllers: [AppController],
       providers: [AppService]
     }).compile()
 
-    appController = app.get<AppController>(AppController)
+    appService = app.get<AppService>(AppService)
   })
+
   it('should be defined"', () => {
-    expect(appController).toBeDefined()
+    expect(appService).toBeDefined()
   })
   it('should return "Hello World!"', () => {
-    expect(appController.getHello()).toBe('Hello World!')
+    expect(appService.getHello()).toBe('Hello World!')
   })
 })
