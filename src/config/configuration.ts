bad2ee0f3b0ea6d43024b73ba6003cb001878454
Mateus Macedo Dos Anjos <mmdatecnologia@gmail.com>
2022-03-29 02:04:58 -0300

    feat(config): database configs

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,15 +1,59 @@
-export const configuration = (): Record<string, unknown> => ({
-  NODE_ENV: process.env.NODE_ENV,
+export interface Configuration {
+  env: 'development' | 'production' | 'test' | string
   app: {
-    name: process.env.APP_NAME || 'NestApplication',
-    description: process.env.APP_DESCRIPTION || 'NestApplication',
-    version: process.env.APP_VERSION || '0.0.1',
-    port: parseInt(process.env.PORT, 10) || 3000
+    name: string
+    description: string
+    version: string
+    port: number
+  }
+  throttler: {
+    ttl: number
+    limit: number
+  }
+  cache: {
+    store: 'redis' | 'memory' | string
+    host: string
+    password: string
+    port: number
+    ttl: number
+  }
+  db: {
+    type: 'mysql' | 'mongo' | string
+    username: string
+    password: string
+    host: string
+    name: string
+    port: number
+    url: string
+  }
+}
+
+export const configuration = (): Configuration => ({
+  env: process.env.NODE_ENV,
+  app: {
+    name: process.env.APP_NAME,
+    description: process.env.APP_DESCRIPTION,
+    version: process.env.APP_VERSION,
+    port: parseInt(process.env.PORT, 10)
+  },
+  throttler: {
+    ttl: parseInt(process.env.THROTTLER_TTL, 10),
+    limit: parseInt(process.env.THROTTLER_LIMIT, 10)
   },
   cache: {
     store: process.env.CACHE_STORE,
     host: process.env.CACHE_HOST,
-    port: process.env.CACHE_PORT,
-    ttl: process.env.CACHE_TTL
+    password: process.env.CACHE_PASSWORD,
+    port: parseInt(process.env.CACHE_PORT, 10),
+    ttl: parseInt(process.env.CACHE_TTL, 10)
+  },
+  db: {
+    type: process.env.DB_TYPE,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    name: process.env.DB_NAME,
+    port: parseInt(process.env.DB_PORT, 10),
+    url: process.env.DB_URL
   }
 })
--- src/config/validation.ts
+++ src/config/validation.ts
@@ -1,15 +1,22 @@
 import * as Joi from 'joi'
 
 export const validationSchema = Joi.object({
-  NODE_ENV: Joi.string().valid('development', 'production', 'test', 'staging').default('development'),
-  APP_NAME: Joi.string().default('NestApplication'),
-  APP_DESCRIPTION: Joi.string().default('NestApplication'),
-  APP_VERSION: Joi.string().required(),
-  PORT: Joi.number().default(3000),
-  CACHE_STORE: Joi.string().valid('redis', 'memory').default('memory'),
-  CACHE_HOST: Joi.string().default('localhost'),
-  CACHE_PORT: Joi.number().default(6379),
-  CACHE_TTL: Joi.number().default(60),
-  THROTTLER_TTL: Joi.number().default(60),
-  THROTTLER_LIMIT: Joi.number().default(100)
+  NODE_ENV: Joi.string().valid('development', 'production', 'test', 'staging'),
+  APP_NAME: Joi.string(),
+  APP_DESCRIPTION: Joi.string(),
+  APP_VERSION: Joi.string(),
+  PORT: Joi.number(),
+  CACHE_STORE: Joi.string().valid('redis', 'memory'),
+  CACHE_HOST: Joi.string(),
+  CACHE_PORT: Joi.number(),
+  CACHE_TTL: Joi.number(),
+  THROTTLER_TTL: Joi.number(),
+  THROTTLER_LIMIT: Joi.number(),
+  DB_TYPE: Joi.string().valid('mysql', 'mongo'),
+  DB_USERNAME: Joi.string(),
+  DB_PASSWORD: Joi.string(),
+  DB_HOST: Joi.string(),
+  DB_NAME: Joi.string(),
+  DB_PORT: Joi.number(),
+  DB_URL: Joi.string()
 })
