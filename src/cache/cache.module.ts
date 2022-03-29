00131077f5ed81a716585daf60546d916ebb205d
Mateus Macedo Dos Anjos <mmdatecnologia@gmail.com>
2022-03-29 02:44:10 -0300

    refactor(cache): cache configs

--- src/cache/cache.module.ts
+++ src/cache/cache.module.ts
@@ -13,8 +13,10 @@ import * as redisStore from 'cache-manager-ioredis'
           return {
             store: redisStore,
             host: configService.get<string>('cache.host'),
-            port: configService.get<number>('cache.port'),
+            port: configService.get<string>('cache.port'),
             ttl: configService.get<number>('cache.ttl'),
+            password: configService.get<string>('cache.password'),
+            prefix: configService.get<string>('cache.prefix'),
             isGlobal: true
           }
         }
