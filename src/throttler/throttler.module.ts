44059d30e4eccbc33ebd30a0c66a126d2f233d5c
Mateus Macedo Dos Anjos <mmdatecnologia@gmail.com>
2022-03-31 17:54:04 -0300

    refactor(throttler): typing interface

--- src/throttler/throttler.module.ts
+++ src/throttler/throttler.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
-import { ThrottlerModule as NestThrottlerModule } from '@nestjs/throttler'
+import { ThrottlerModule as NestThrottlerModule, ThrottlerModuleOptions } from '@nestjs/throttler'
 
 @Module({
   imports: [
     NestThrottlerModule.forRootAsync({
       inject: [ConfigService],
-      useFactory: async (configService: ConfigService): Promise<any> => {
+      useFactory: async (configService: ConfigService): Promise<ThrottlerModuleOptions> => {
         return {
           ttl: configService.get<number>('throttler.ttl'),
           limit: configService.get<number>('throttler.limit')
