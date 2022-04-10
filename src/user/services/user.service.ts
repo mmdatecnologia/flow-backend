55dae80a125f0a7d302e4fe82c60e244f0985c55
Mateus Macedo Dos Anjos <mmdatecnologia@gmail.com>
2022-04-10 14:54:35 -0300

    feat(user): ensure create and persiste a user

--- src/user/services/user.service.ts
+++ src/user/services/user.service.ts
@@ -1,11 +1,10 @@
 import { CreateUserDto } from '@app/user/models/dtos/create-user.dto'
 import { GetUserDto } from '@app/user/models/dtos/get-user.dto'
 import { UserEntity } from '@app/user/models/user.entity'
+import { UseCase } from '@app/user/services/service.interface'
 import { Injectable } from '@nestjs/common'
 import { InjectRepository } from '@nestjs/typeorm'
 import { MongoRepository } from 'typeorm'
-
-import { UseCase } from './service.interface'
 
 @Injectable()
 export class UserService implements UseCase.CreateUser {
@@ -14,6 +13,9 @@ export class UserService implements UseCase.CreateUser {
     private readonly userRepository: MongoRepository<UserEntity>
   ) {}
   async create(user: CreateUserDto): Promise<GetUserDto> {
-    throw new Error(user + ' is not provided')
+    const newUserEntity = Object.assign(new UserEntity(), user)
+    const userCreated = await this.userRepository.save<UserEntity>(newUserEntity)
+    const { id, email } = userCreated
+    return Object.assign(new GetUserDto(), { id, email })
   }
 }
