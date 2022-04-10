6e8c86ecc091298674b88239fe5f460a61c3afbe
Mateus Macedo Dos Anjos <mmdatecnologia@gmail.com>
2022-04-10 14:57:36 -0300

    refactor(user): move tests to feat context

--- src/user/services/user.service.spec.ts
+++ src/user/services/user.service.spec.ts
@@ -20,38 +20,40 @@ describe('UserService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined()
   })
-  it('should call repository with correct params', async () => {
-    userRepository.save = jest.fn().mockResolvedValue(new UserEntity())
-    const user = new CreateUserDto()
+  describe('create', () => {
+    it('should call repository with correct params', async () => {
+      userRepository.save = jest.fn().mockResolvedValue(new UserEntity())
+      const user = new CreateUserDto()
 
-    await service.create(user)
+      await service.create(user)
 
-    expect(userRepository.save).toHaveBeenCalledTimes(1)
-    expect(userRepository.save).toHaveBeenCalledWith(user)
-  })
-  it('should return a GetUserDto instance with correct data', async () => {
-    const mockedUserEmail = faker.internet.email()
-    const mockedUserId = faker.random.uuid()
-    const createUserDto = new CreateUserDto()
-    const mockedUserEntity = new UserEntity()
-
-    mockedUserEntity.id = mockedUserId
-    mockedUserEntity.email = mockedUserEmail
-    jest.spyOn(userRepository, 'save').mockResolvedValue(mockedUserEntity)
-
-    createUserDto.email = mockedUserEmail
-    const userCreated = await service.create(createUserDto)
-
-    expect(userCreated).toBeInstanceOf(GetUserDto)
-    expect(userCreated.email).toBe(createUserDto.email)
-    expect(userCreated).toHaveProperty('id')
-    expect(userCreated.id).toBe(mockedUserId)
-  })
-  it('should throw an error if repository throws an error', async () => {
-    jest.spyOn(userRepository, 'save').mockImplementationOnce(() => {
-      throw new Error()
+      expect(userRepository.save).toHaveBeenCalledTimes(1)
+      expect(userRepository.save).toHaveBeenCalledWith(user)
     })
-    const createUserDto = new CreateUserDto()
-    expect(service.create(createUserDto)).rejects.toThrow()
+    it('should return a GetUserDto instance with correct data', async () => {
+      const mockedUserEmail = faker.internet.email()
+      const mockedUserId = faker.random.uuid()
+      const createUserDto = new CreateUserDto()
+      const mockedUserEntity = new UserEntity()
+
+      mockedUserEntity.id = mockedUserId
+      mockedUserEntity.email = mockedUserEmail
+      jest.spyOn(userRepository, 'save').mockResolvedValue(mockedUserEntity)
+
+      createUserDto.email = mockedUserEmail
+      const userCreated = await service.create(createUserDto)
+
+      expect(userCreated).toBeInstanceOf(GetUserDto)
+      expect(userCreated.email).toBe(createUserDto.email)
+      expect(userCreated).toHaveProperty('id')
+      expect(userCreated.id).toBe(mockedUserId)
+    })
+    it('should throw an error if repository throws an error', async () => {
+      jest.spyOn(userRepository, 'save').mockImplementationOnce(() => {
+        throw new Error()
+      })
+      const createUserDto = new CreateUserDto()
+      expect(service.create(createUserDto)).rejects.toThrow()
+    })
   })
 })
